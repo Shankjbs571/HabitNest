35d287173e80ddfb9e480835b58ad1a508e26706
Lanthoiba2022 <khumanthemlanthoiba@gmail.com>
2024-07-28 16:23:59 +0530

    Logo added

---
+++ Frontend/habitnest/src/components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div className="fixed top-0 left-0 m-4">
+      <h1 className="text-2xl font-bold text-gray-800">HabitNest</h1>
+    </div>
+  );
+};
+
+export default Logo;
