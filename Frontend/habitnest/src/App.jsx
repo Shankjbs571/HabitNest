38662be318eec6bf3443161f699a7eb824b4ada6
Lanthoiba2022 <khumanthemlanthoiba@gmail.com>
2024-07-28 16:33:08 +0530

    undone logo

--- Frontend/habitnest/src/App.jsx
+++ Frontend/habitnest/src/App.jsx
@@ -3,14 +3,12 @@ import './App.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import { Signin } from './pages/Signin';
 import { Signup } from './pages/Signup';
-import Logo from './components/Logo';
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <Logo/>
         <Routes>
         <Route path="/" element={<Signup />}/>
         <Route path="/signin" element={<Signin />}/>
