94d73fd1361c89e98a50c701a7d8b083502f06a0
shankjbs571 <shankjbs571@gmail.com>
2024-07-28 17:27:46 +0530

    deletes

--- Frontend/habitnest/src/pages/SignIn.jsx
+++
@@ -1,3 +0,0 @@
-export default function SignIn() {
-    return <h2 className="text-2xl font-bold">Sign In Page</h2>;
-  }
--- Frontend/habitnest/src/pages/SignUp.jsx
+++
@@ -1,3 +0,0 @@
-export default function SignUp() {
-    return <h2 className="text-2xl font-bold">Sign Up Page</h2>;
-  }
